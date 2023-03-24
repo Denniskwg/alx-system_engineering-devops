de22ce5aa3d5aa0a33e8f67bc20ca80cb5cefb00
Dennis Kamau <dkamau476@gmail.com>
2023-03-24 11:34:24 -0700

    adding solution for question 2

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+#kills a process called killmenow
+exec { 'killmenow':
+  command => 'pkill -f killmenow',
+  path    => ['/bin', '/usr/bin', '/usr/local/bin'],
+  onlyif  => 'pgrep -f killmenow',
+}
