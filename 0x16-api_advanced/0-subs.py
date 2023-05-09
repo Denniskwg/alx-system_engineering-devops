7c70907eacf74922ff00fdb01ca1d5238d7aca4f
Denniskwg <dkamau476@gmail.com>
2023-05-09 14:06:56 +0300

    adding solution for question 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""0-subs defines a function number_of_subscribers that queries the
+reddit api and returns number of subscribers for a specific subreddit
+"""
+
+
+import requests
+import json
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for subreddit"""
+    headers = {'user-Agent': 'dennisrequest'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    req = requests.get(url, headers=headers)
+    if req.status_code == 302 or req.status_code == 404:
+        return 0
+    req = req.json()
+    if 'error' in req:
+        return 0
+    elif req.get('data', None) is not None:
+        res = req.get('data').get('subscribers', None)
+        if res is not None:
+            return res
+        else:
+            return 0
